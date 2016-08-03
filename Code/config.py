1b3b3c73fc85f30141b349317cd1131339fc98a4
wronk <wronk.mark@gmail.com>
2016-08-03 11:32:31 -0700

    Add docs, add functionality for simulation

--- Code/config.py
+++ Code/config.py
@@ -11,8 +11,10 @@ import numpy as np
 MEAN_PARAMS = dict(score_mat_order=['Time window', 'Subject index',
                                     'Cross-validation index', 'Kernel index',
                                     'C-val index', 'Gamma val index'])
+# Manuscript:
 
-CSP_PARAMS = dict(reg_list=['ledoit_wolf', 'oas', .05, .1, .15, .2, .25],
+#CSP_PARAMS = dict(reg_list=['ledoit_wolf', 'oas', 0.05, 0.1, 0.15, 0.2, 0.25],  # Experimental Data
+CSP_PARAMS = dict(reg_list=['ledoit_wolf', 'oas', 0.15, 0.2, 0.25],  # Simulation Data
                   n_components_list=range(1, 6),
                   score_mat_order=['Time window', 'Regularization',
                                    'Number of components', 'Subject index',
@@ -21,7 +23,8 @@ CSP_PARAMS = dict(reg_list=['ledoit_wolf', 'oas', .05, .1, .15, .2, .25],
 
 PCA_PARAMS = dict(n_components_list=range(1, 6))
 
-ICA_PARAMS = dict(n_components_list=[.85, .9, .95, .99],
+#ICA_PARAMS = dict(n_components_list=[0.85, 0.9, 0.95, 0.99],  # Experimental Data
+ICA_PARAMS = dict(n_components_list=[0.95, 0.99, 0.995],  # Simulated Data
                   ica_init_params=dict(method='extended-infomax',
                                        max_iter=50000))
 
@@ -37,6 +40,6 @@ DATA_PARAMS = dict(roi='RTPJAnatomical-rh.label',
                    lambda2=1. / 9.,  # Standard regularization
                    eq='EQ',  # 'DQ' or 'EQ' Just disqualified trials or equalized trials
                    loaded_bin_width=None,
-                   useAbsInMorph=False,
+                   useAbsInMorph=False,  # Use absolute value of dipole currents
                    patterns=['space_maintain', 'space_switch'],  # Maintain should be first!
                    hemi_idx=1)  # 0 = left hemisphere; 1 = right hemisphere
